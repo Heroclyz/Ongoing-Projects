01ed3d639e7314885aadbbd578e6faaca42998bb
Ege Öner <egeonerss45.60@gmail.com>
2026-07-01 20:57:16 +0300

    Learning Java

---
+++ Learning Java/q1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class q1 {
+    public static void main(String[] args)
+    {
+        System.out.print("Isminizi Giriniz:");
+        Scanner input = new Scanner(System.in);
+        String adSoyad = input.nextLine();
+        System.out.println("Adiniz Soyadiniz:" + adSoyad);
+        input.close();
+    }
+}
