5600ba9dda52f5f45ca504a2b91bb9521864d675
Ege Öner <egeonerss45.60@gmail.com>
2026-07-02 15:57:33 +0300

    Learning More About Abstract Class-Methods

--- LearningJava/q12.java
+++ LearningJava/q12.java
@@ -1,3 +1,6 @@
+
+/* 
+
 package LearningJava;
 
 abstract class Language
@@ -15,5 +18,53 @@ public class q12 extends Language{
 
         object.display();
 
+    }
+}
+*/
+
+//Abstract Class - Method --> Learning More
+
+
+package LearningJava;
+
+
+abstract class Animal12
+{
+    abstract void makeSound();
+
+    public void eat()
+    {
+        System.out.println("All Animals Eat Food");
+    }
+
+}
+
+class Dog12 extends Animal12
+{
+    public void makeSound()
+    {
+        System.out.println("Dogs Bark!");
+    }
+}
+class Cat extends Animal12
+{
+    public void makeSound()
+    {
+        System.out.println("Cats Meow");
+    }
+}
+
+public class q12
+{
+    public static void main(String[] args)
+    {
+        Dog12 dog = new Dog12();
+        Cat cat = new Cat();
+
+        dog.makeSound();
+        dog.eat();
+        System.out.println("----------------------");
+        cat.makeSound();
+        cat.eat();
     }
 }
