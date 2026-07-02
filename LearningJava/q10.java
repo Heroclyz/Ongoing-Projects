89da5d1f29b8b855f955d6ac15c9f86d26809d61
Ege Öner <egeonerss45.60@gmail.com>
2026-07-02 15:30:57 +0300

    Learning Inheritance in Java

--- LearningJava/q10.java
+++ LearningJava/q10.java
@@ -1,3 +1,6 @@
+
+
+/*
 package LearningJava;
 
 class Animals
@@ -30,5 +33,74 @@ public class q10 {
         alex.friendly();
         alex.displayName();
 
+    }
+}
+    */
+// Sending Data to Class with Scanner-getter-setter commands
+
+package LearningJava;
+
+import java.util.Scanner;
+
+class Animal 
+{
+    private String name;
+    private int age;
+
+    public void setname(String name)
+    {
+        this.name = name;
+    }
+    public String getname()
+    {
+        return name;
+    }
+
+    public void setage(int age)
+    {
+        this.age = age;
+    }
+    
+    public int getage()
+    {
+        return age;
+    }
+
+}
+
+class Dog extends Animal
+{
+    public void display()
+    {
+        System.out.println("My Dogs Name Is = " + getname());
+        System.out.println("My Dogs Age Is = " + getage());
+    }
+
+    public void bark()
+    {
+        System.out.println(getname() +" Barks A LOT!");
+    }
+
+}
+
+public class q10 {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please Enter Your Dogs Name:");
+        String name = input.nextLine();
+
+        System.out.print("Please Enter Your Dogs Age:");
+        int age = input.nextInt();
+
+        Dog dog1 = new Dog();
+
+        dog1.setage(age);
+        dog1.setname(name);
+
+        dog1.display();
+        dog1.bark();
+        input.close();
     }
 }
