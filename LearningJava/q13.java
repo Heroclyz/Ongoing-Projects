fd7d985c3a2d6c0ccd9de5c2b4df41eed671ac77
Ege Öner <egeonerss45.60@gmail.com>
2026-07-05 23:36:33 +0300

    Learning Java Interface

---
+++ LearningJava/q13.java
@@ -0,0 +1,23 @@
+package LearningJava;
+
+interface area
+{
+    void CalculateArea(int s, int l);
+}
+
+class Home
+{
+    public void CalculateArea(int s, int l)
+    {
+        double area = s*l;
+        System.out.println("-> " + area);
+    }
+}
+public class q13
+{
+    public static void main(String[] args)
+    {
+        Home home1 = new Home();
+        home1.CalculateArea(3, 4);
+    }
+}
