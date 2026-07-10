3466286d5f058ca939a76693e38780886a728478
Ege Öner <egeonerss45.60@gmail.com>
2026-07-10 22:16:44 +0300

    Use Of Enum

---
+++ LearningJava/q18.java
@@ -0,0 +1,16 @@
+package LearningJava;
+
+enum Size
+{
+    SMALL,  MEDİUM, LARGE, EXTRALARGE
+}
+
+public class q18 {
+    public static void main(String[] args)
+    {
+        System.out.println(Size.EXTRALARGE);
+        System.out.println(Size.LARGE);
+        System.out.println(Size.MEDİUM);
+        System.out.println(Size.SMALL);
+    }
+}
