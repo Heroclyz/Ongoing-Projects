ddbabd686a56f8c20bc9c61413acaa85a94155c7
Ege Öner <egeonerss45.60@gmail.com>
2026-07-12 22:47:40 +0300

    How to use ArrayList

---
+++ LearningJava/q20.java
@@ -0,0 +1,18 @@
+package LearningJava;
+
+import java.util.ArrayList;
+
+public class q20 {
+    public static void main(String[] args)
+    {
+        ArrayList<String> languages = new ArrayList<>();
+
+        languages.add("C");
+        languages.add("Java");
+        languages.add("C#");
+        languages.add("JavaScript");
+        languages.add("Python");
+
+        System.out.println(languages + " ");
+    }
+}
